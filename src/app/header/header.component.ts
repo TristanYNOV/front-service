6e8f9f8879d0a3407bb38d2ea87cb203e44dbb68
TristanEPSI <tristan.gottschak@laposte.net>
2025-08-14 18:49:02 +0200

    remove: remove Mat-Icon

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,10 @@
 import {Component, inject, Input} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {AuthModalComponent} from '../core/shared/modals/auth/auth-modal.component';
-import {MatIcon} from '@angular/material/icon';
-import {MatIconButton} from '@angular/material/button';
-
-
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  imports: [MatIcon, MatIconButton],
   standalone: true,
 })
 export class HeaderComponent {

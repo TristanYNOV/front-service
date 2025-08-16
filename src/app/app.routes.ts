50caca29c6ffc7ea6758082c50f0f813063a8839
Tristan Gottschalk <123099307+TristanYNOV@users.noreply.github.com>
2025-08-16 20:51:59 +0200

    feat: add welcome page (#24)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,13 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
 import { DiscoverComponent } from './pages/discover/discover.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 import { authGuard } from './core/guards/auth.guard';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'discover', component: DiscoverComponent },
-  { path: 'welcome', component: DiscoverComponent, canActivate: [authGuard] },
+  { path: 'welcome', component: WelcomeComponent, canActivate: [authGuard] },
   { path: '**', component: NotFoundComponent },
 ];

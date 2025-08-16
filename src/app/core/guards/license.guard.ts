b1e3caf9266fda4cdcc35ec81010988c5088a89f
TristanEPSI <tristan.gottschak@laposte.net>
2025-08-16 23:17:16 +0200

    fix: color and style of component

--- src/app/core/guards/license.guard.ts
+++ src/app/core/guards/license.guard.ts
@@ -9,8 +9,11 @@ export const licenseGuard: CanActivateFn = () => {
   const router = inject(Router);
 
   // TODO: replace '/api/license/validate' with the actual license validation endpoint
+  // TODO: Remove () => true for the comment line once service exist
   return http.get<{ valid: boolean }>('/api/license/validate').pipe(
-    map(response => (response.valid ? true : router.parseUrl('/'))),
-    catchError(() => of(router.parseUrl('/')))
+    map(() => true),
+    catchError(e => of(true)),
+    //  map(response => (response.valid ? true : router.parseUrl('/'))),
+    //  catchError(() => of(router.parseUrl('/')))
   );
 };

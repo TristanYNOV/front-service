348727e59c2a03dfa907bda196eb561ec128c76c
Tristan Gottschalk <123099307+TristanYNOV@users.noreply.github.com>
2025-08-16 21:18:11 +0200

    feat: add service unavailable component

---
+++ src/app/components/service-unavailable/service-unavailable.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-unavailable',
+  standalone: true,
+  templateUrl: './service-unavailable.component.html',
+})
+export class ServiceUnavailableComponent {}

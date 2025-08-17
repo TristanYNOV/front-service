f634c4e9530b3c5fcee162e0388709d31ec6f138
Tristan Gottschalk <123099307+TristanYNOV@users.noreply.github.com>
2025-08-17 17:23:11 +0200

    feat: add draggable analysers (#36)

--- src/app/pages/analyse/analyse.component.ts
+++ src/app/pages/analyse/analyse.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { VideoPlayerComponent } from './video-player.component';
+import { SequencerPanelComponent } from './sequencer-panel.component';
+import { TimelineComponent } from './timeline.component';
 
 @Component({
   selector: 'app-analyse',
   standalone: true,
   templateUrl: './analyse.component.html',
+  imports: [VideoPlayerComponent, SequencerPanelComponent, TimelineComponent],
 })
 export class AnalyseComponent {}

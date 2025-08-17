9b72fc06a5d00ff67d1a2652f49d442494ffa885
Tristan Gottschalk <123099307+TristanYNOV@users.noreply.github.com>
2025-08-17 17:07:18 +0200

    feat: toggle sidebar on analyse route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component, inject} from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Store } from '@ngrx/store';
 import {MiniComponent} from './components/data-item-container/Minified/mini.component';
 import {selectIdleItems, selectSavedItems} from './store/Data/dataState.selectors';
 import { loadInitialState } from './store/User/user.actions';
+import {filter} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,9 +16,19 @@ import { loadInitialState } from './store/User/user.actions';
 })
 export class AppComponent {
   private readonly store = inject(Store);
+  private readonly router = inject(Router);
+
+  showSidebar = true;
 
   constructor() {
     this.store.dispatch(loadInitialState());
+
+    this.showSidebar = !this.router.url.startsWith('/analyse');
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe(({urlAfterRedirects}) => {
+        this.showSidebar = !urlAfterRedirects.startsWith('/analyse');
+      });
   }
 
   get idleItems() {

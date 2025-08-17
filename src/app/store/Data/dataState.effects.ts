7d9d818adc0650cd3b49fcc923acb65daa5305f2
TristanEPSI <tristan.gottschak@laposte.net>
2025-08-17 14:33:26 +0200

    fix build and add routing to the logo

--- src/app/store/Data/dataState.effects.ts
+++ src/app/store/Data/dataState.effects.ts
@@ -3,6 +3,7 @@ import { createEffect } from '@ngrx/effects';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter, switchMap } from 'rxjs';
 import { loadDiscoverData, clearIdleData } from './dataState.actions';
+import {Action} from '@ngrx/store';
 
 @Injectable()
 export class DataEffects {
@@ -12,7 +13,7 @@ export class DataEffects {
     this.router.events.pipe(
       filter((event): event is NavigationEnd => event instanceof NavigationEnd),
       switchMap(({ urlAfterRedirects }) => {
-        const actions = [clearIdleData()];
+        let actions: Array<Action> = [clearIdleData()];
         switch (urlAfterRedirects) {
           case '/discover':
             actions.push(loadDiscoverData());

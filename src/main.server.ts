b46de04a11b7e6a7dd8c9ab86146a8de5f339917
Tristan Gottschalk <123099307+TristanEPSI@users.noreply.github.com>
2025-12-15 06:55:09 +0100

    fix packages & comment

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,5 +1,4 @@
-// ✅ Version corrigée
-import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
+import {bootstrapApplication, BootstrapContext} from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 

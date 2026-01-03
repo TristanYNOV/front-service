4eff88a855195a8d4aea11d1313e16a301db8125
TristanEPSI <tristan.gottschak@laposte.net>
2026-01-03 17:09:21 +0100

    fix: display video HTML and ts, remove old container for video

--- src/app/components/analyse/video/video-player.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-video-player',
-  standalone: true,
-  templateUrl: './video-player.component.html',
-  styleUrl: './video-player.component.scss',
-})
-export class VideoPlayerComponent {}

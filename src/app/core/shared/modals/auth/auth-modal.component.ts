31a0df83f68c6877486d433bc98ec91c1aacaae4
TristanEPSI <tristan.gottschak@laposte.net>
2026-03-23 12:19:25 +0100

    fix build

--- src/app/core/shared/modals/auth/auth-modal.component.ts
+++ src/app/core/shared/modals/auth/auth-modal.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import {MatError, MatFormField, MatHint, MatLabel} from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -31,6 +31,7 @@ type AuthForm = FormGroup<{
   templateUrl: './auth-modal.component.html',
   imports: [
     MatFormField,
+    MatHint,
     MatLabel,
     MatInput,
     MatError,

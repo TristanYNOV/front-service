40a84d0b0cc65f1260f9ca76255d7395a55d973c
Tristan Gottschalk <123099307+TristanEPSI@users.noreply.github.com>
2026-05-26 11:12:13 +0200

    fix: lint

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideTransloco, translocoConfig } from '@jsverse/transloco';
